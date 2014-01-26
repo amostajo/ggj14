a681c8507397787d28ccb513bd0f0b063a70b948
Alejandro Mostajo <amostajo@amsgames.com>
2014-01-26 02:59:51 -0600

    Proyectiles

--- Assets/Scripts/Boss.js
+++ Assets/Scripts/Boss.js
@@ -5,13 +5,15 @@
  */
 class Boss extends MonoBehaviour {
 
+  public var projectiles : List.<BossProjectile>;
+
   /**
    * Ataca al player con un poder determinado.
    *
    * @param Player.Power power Poder de ataque.
    */
-  public function Attack (power : Player.Power) {
-
+  public function Attack (power : Player.Power, target : Transform) {
+    projectiles[power - 1].Shoot(target);
   }
 
 }
