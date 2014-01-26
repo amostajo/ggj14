ba1e088e65bee71799117d7a17a66f6762df198b
Alejandro Mostajo <amostajo@amsgames.com>
2014-01-25 19:49:37 -0600

    Obstacle triggers.

---
+++ Assets/Scripts/ObstacleFire.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+/**
+ * Fire obstacle
+ */
+class ObstacleFire extends ObstacleTrigger {
+
+  /**
+   * Inicializa el obstaculo como the fuego.
+   */
+  public function Awake () {
+    super.Awake();
+    power = Player.Power.Fire;
+  }
+
+}
---
+++ Assets/Scripts/ObstacleWater.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+/**
+ * Fire obstacle
+ */
+class ObstacleWater extends ObstacleTrigger {
+
+  /**
+   * Inicializa el obstaculo como the agua.
+   */
+  public function Awake () {
+    super.Awake();
+    power = Player.Power.Water;
+  }
+
+}
