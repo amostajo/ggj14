8d89011d5bc9288661ce2eea9ca02c2105f61de8
Alejandro Mostajo <amostajo@amsgames.com>
2014-01-26 08:58:01 -0600

    Projectile delete, my scene save

--- Assets/Scripts/Boss.js
+++ Assets/Scripts/Boss.js
@@ -5,15 +5,12 @@
  */
 class Boss extends MonoBehaviour {
 
-  public var projectiles : List.<BossProjectile>;
-
   /**
    * Ataca al player con un poder determinado.
    *
    * @param Player.Power power Poder de ataque.
    */
-  public function Attack (power : Player.Power, target : Transform) {
-    projectiles[power - 1].Shoot(target);
+  public function Attack (power : Player.Power) {
   }
 
 }
