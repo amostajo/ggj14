6043f7277eadea490de89c80b089ce30a933c61d
Malavasi <f.adrian59@yahoo.com>
2014-02-26 18:20:30 -0600

    Adjust player killer trigger

---
+++ Assets/Scripts/PlayerKillerPosition.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+/**
+ * 
+ * Puts a killer on the left of the screen 
+ *
+ * @author Adrian Fernandez <fadrian59@gmail.com>
+ */
+public class PlayerKillerPosition extends MonoBehaviour { 
+	/**
+   	 * Start
+   	 * Puts a killer on the left of the screen 
+   	 */
+	function Start () {
+		var width:float =Screen.width; 
+		var height:float =Screen.height; 
+		transform.position.x = -1*(width/height*1.6+6.5);
+	}
+}
