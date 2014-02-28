87f700f22be659d4507230798d96c64d345b0a92
Malavasi <f.adrian59@yahoo.com>
2014-02-27 22:08:00 -0600

    Fixed the PlayerKillerPosition script

--- Assets/Scripts/PlayerKillerPosition.js
+++ Assets/Scripts/PlayerKillerPosition.js
@@ -6,11 +6,22 @@
  * @author Adrian Fernandez <fadrian59@gmail.com>
  */
 public class PlayerKillerPosition extends MonoBehaviour { 
+
+	/**
+   	 * The extra factor to fix the object position with the relation width-height of the screen
+     */
+	public var ratio : float = 1.6f;
+	
+	/**
+   	 * The constant that move the object to a standard position
+     */
+	public var adjustment : float = 6.5f;
+
 	/**
    	 * Start
    	 * Puts a killer on the left of the screen 
    	 */
 	function Start () {
-		transform.position.x = -1.0f*(parseFloat(Screen.width)/parseFloat(Screen.height)*1.6f+6.5f);
+		transform.position.x = -1.0f*(parseFloat(Screen.width)/parseFloat(Screen.height)*ratio+adjustment);
 	}
 }
