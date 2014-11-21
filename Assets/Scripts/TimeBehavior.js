07d2a1deffdbb9fdf6a369598d8694ad5a7d7db1
Alejandro Mostajo <amostajo@amsgames.com>
2014-11-21 17:30:02 -0600

    20141121 .- Refactoring
    Comments added and indicated when using float variables.

--- Assets/Scripts/TimeBehavior.js
+++ Assets/Scripts/TimeBehavior.js
@@ -1,25 +1,30 @@
 ﻿#pragma strict
+/**
+ * Input manager. Handles all inputs in the game.
+ *
+ * @author Adrian Fernandez
+ */
+class TimeBehavior extends MonoBehaviour {
 
-function Start () {
-    
+  /**
+   * Start.
+   */
+  public function Start () {
+      
     var time: System.DateTime = System.DateTime.Now;
-    if(time.Hour<14 && time.Hour>6)
-    {
+    if(time.Hour<14 && time.Hour>6) {
         light.color = Color.white;
     }
-    if(time.Hour<18 && time.Hour>14)
-    {
-        light.color = new Color(0.9254,0.6,0.1921);
+    if(time.Hour<18 && time.Hour>14) {
+        light.color = new Color(0.9254f,0.6f,0.1921f);
     }
-    if(time.Hour<24 && time.Hour>18)
-    {
-        light.color = new Color(0.236,0.153,0.49);
+    if(time.Hour<24 && time.Hour>18) {
+        light.color = new Color(0.236f,0.153f,0.49f);
     }
-    if(time.Hour<6 && time.Hour>0)
-    {
-        light.color = new Color(0.236,0.153,0.49);
+    if(time.Hour<6 && time.Hour>0) {
+        light.color = new Color(0.236f,0.153f,0.49f);
     }
 
+  }
 
 }
-
